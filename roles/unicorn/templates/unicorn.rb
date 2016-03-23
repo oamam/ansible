9255f956e5e6e6e12f6e7605567e634f5b0f0364
masashi0127 <chapa0106@gmail.com>
2016-03-24 00:19:31 +0900

    edit playbook for ruby

--- roles/unicorn/templates/unicorn.rb
+++ roles/unicorn/templates/unicorn.rb
@@ -4,5 +4,5 @@ timeout 15
 listen "/var/run/unicorn.sock"
 pid "/var/run/unicorn.pid"
 
-stderr_path "{{ app_home_dir }}/log/unicorn_error.log"
-stdout_path "{{ app_home_dir }}/log/unicorn.log"
+stderr_path "{{ app_home_dir }}/{{ app_name }}/log/unicorn_error.log"
+stdout_path "{{ app_home_dir }}/{{ app_name }}/log/unicorn.log"
